8f92d7059357655697cc834b4e36ef125daaab67
NaiyinZhao <cottonyin2012@gmail.com>
2014-03-14 09:48:32 +0800

    rollback

--- src/main/java/com/onlinejudge/judge/controller/ContestController.java
+++ src/main/java/com/onlinejudge/judge/controller/ContestController.java
@@ -19,7 +19,6 @@ public class ContestController {
 
 	@RequestMapping(value = "", method = RequestMethod.GET)
 	public String home(Model model) {
-		//NaiyinZhao test
 		int count = contestService.count();
 		model.addAttribute("count", count);
 		return "/contest/contest";
